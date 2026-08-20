e926ad6d638a540fa67584c583e49ba44d1bfb07
jessbenitez <beniteza.jesica@gmail.com>
2026-08-19 21:40:07 -0300

    agregado de metodos de validacion

--- clase_3/Ejercicio 6.py
+++ clase_3/Ejercicio 6.py
@@ -29,3 +29,22 @@ print("Largo de la candena: ", len(texto))
 
 
 
+
+
+
+
+"""Analicen además los métodos de validación con las cadenas "Programacion", "2026", "Python3" y "Programacion en 
+Python": isalpha(), isdigit() e isalnum(). Expliquen por qué isalpha() retorna False cuando la cadena contiene espacios, 
+aunque todas sus palabras estén formadas por letras.
+
+
+
+* Metodos de validacion que preguntan por el tipo de contenido de una cadena
+isalpha() = consulta si es texto, ejemplo:  "Programacion", "Programacion en 
+Python"
+
+isdigit() = consulta si en son solo numeros, ejemplo: "2026"
+isalnum() = consulta si es alfanumerico, ejemplo: "Python3"
+isalnum() retornara False cuando contenga espacios ya que este no se considera como un caracter alfanumerico.
+
+
