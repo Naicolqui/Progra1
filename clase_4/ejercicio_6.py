f8be52bf1968af618a495c92d3276e93dacaa77b
Nicole Quilmore <nicolequilmore96@gmail.com>
2026-08-26 19:45:17 -0300

    Ejercicio 6

--- clase_4/ejercicio_6.py
+++ clase_4/ejercicio_6.py
@@ -1,9 +1,16 @@
 
-
-
+def recorrer_alumnos(alumnos):
+    for alumno in alumnos:
+        print(f"{alumno[0]} nació el día: {alumno[1][0]}-{alumno[1][1]}-{alumno[1][2]}")
 
 def main ():
     alumnos = (
         ("Ana", (12, "Marzo", 2005)),
         ("Bruno", (8, "Julio", 2004)),
         )
+
+    print(f"Ana cumple años en: , {alumnos[0][1][1]}")
+    recorrer_alumnos(alumnos)
+
+if __name__ == "__main__":
+    main()
